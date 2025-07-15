13b301a2c426f4d242b07144da3ddacaefda69ee
ZevHadid <zevhadid@gmail.com>
2025-07-15 19:16:56 +0700

    Added frontend url and allowed angular frontend

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: config.get<string>('FRONTEND_URL'), // <-- allow Angular frontend
+    credentials: true,
+  });
+  
   app.use(cookieParser());
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
